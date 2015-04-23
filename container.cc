69cb440a38c788849159cb4db1cf9ed7f39bcc22
bluebore <shiguang31@163.com>
2015-04-23 15:55:19 +0800

    Add rootfs

--- container.cc
+++ container.cc
@@ -4,14 +4,16 @@
 //
 // Author: yanshiguang02@baidu.com
 
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mount.h>
 #include <stdio.h>
 #include <sched.h>
 #include <signal.h>
-#include <unistd.h>
-#include <errno.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define CLONE_NEWUTS                    0x04000000                /* New utsname group? */
 #define CLONE_NEWIPC                    0x08000000                /* New ipcs */
@@ -32,10 +34,43 @@ char* container_args[] = {
 
 int container_main(void* arg)
 {
-    printf("Container - inside the container!\n");
-
+    printf("Container [%5d] - inside the container!\n", getpid());
     sethostname("container", 10);
+
+    // proc
+    if (mount("proc", "rootfs/proc", "proc", 0, NULL) !=0 ) {
+        perror("proc");
+    }
+    // sysfs
+    if (mount("sysfs", "rootfs/sys", "sysfs", 0, NULL)!=0) {
+        perror("sys");
+    }
+    // tmpfs
+    if (mount("none", "rootfs/tmp", "tmpfs", 0, NULL)!=0) {
+        perror("tmp");
+    }
+    // udev
+    if (mount("udev", "rootfs/dev", "devtmpfs", 0, NULL)!=0) {
+        perror("dev");
+    }
+    // devpts
+    if (mount("devpts", "rootfs/dev/pts", "devpts", 0, NULL)!=0) {
+        perror("dev/pts");
+    }
+    // shm
+    if (mount("shm", "rootfs/dev/shm", "tmpfs", 0, NULL)!=0) {
+        perror("dev/shm");
+    }
+    // run
+    if (mount("tmpfs", "rootfs/run", "tmpfs", 0, NULL)!=0) {
+        perror("run");
+    }
+    // chroot
+    if ( chdir("./rootfs") != 0 || chroot("./") != 0 ){
+        perror("chdir/chroot");
+    }
     execv(container_args[0], container_args); 
+    perror("exec");
     printf("Something's wrong!\n");
     return 1;
 }
@@ -44,7 +79,7 @@ int main()
 {
     printf("Parent - start a container!\n");
     // clone
-    int container_pid = clone(container_main, container_stack+STACK_SIZE, CLONE_NEWUTS | SIGCHLD, NULL);
+    int container_pid = clone(container_main, container_stack+STACK_SIZE, CLONE_NEWNET | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWUTS | SIGCHLD, NULL);
 
     if (container_pid == -1) {
         fprintf(stderr, "Clone fail: %s\n", strerror(errno));
